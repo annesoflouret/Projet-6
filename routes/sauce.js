48b79d4e3cd3f0d942dcd7c511423db612b4f2f6
annesoflouret <annesophieflouret@gmail.com>
2020-07-22 13:50:24 +0200

    routes construction sauce and user

---
+++ routes/sauce.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const sauceCtrl = require('../controllers/sauce');
+
+router.post('/', /*auth,*/sauceCtrl.createSauce);
+router.get('/:id', /*auth,*/ sauceCtrl.getOneSauce);
+router.get('/', /*auth,*/ sauceCtrl.listSauces);
+router.put('/:id', /*auth,*/multer, sauceCtrl.updateSauce);
+router.delete('/:id', /*auth,*/ sauceCtrl.deleteSauce); 
+//router.post('/:id/like', auth, sauceCtrl.likeSauces);
+
+module.exports = router;
+
+
