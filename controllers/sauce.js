fb03ca5bf7a5823f2ab5cf70830a369ceba31cf6
annesoflouret <annesophieflouret@gmail.com>
2020-07-22 13:51:40 +0200

    controllers construction

---
+++ controllers/sauce.js
@@ -0,0 +1,53 @@
+const Sauce = require('../models/Sauce');
+
+exports.createSauce = (req, res, next) => {
+    console.log(req);
+    const sauceObjet = JSON.parse(req.body.sauce);
+
+    delete sauceObjet._id;
+   
+    const sauce = new Sauce({
+        ...sauceObjet,
+        likes: 0,
+        dislikes: 0,
+        usersLiked: [],
+        usersDisliked: [],
+        //imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+    sauce.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+        .catch(error => res.status(400).json({ error }));
+};
+
+
+
+exports.getOneSauce =  (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+};
+
+exports.listSauces =  (req, res, next) => {
+    Sauce.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.updateSauce =  (req, res, next) => {
+    const sauceObject = req.file ?
+    {
+      ...JSON.parse(req.body.sauce),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+    
+    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.deleteSauce =  (req, res, next) => {
+    Sauce.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+};
+
