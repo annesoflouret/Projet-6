a2f01d28e5f143a28564c00930d8594f9b1ab66d
annesoflouret <annesophieflouret@gmail.com>
2020-07-28 23:19:58 +0200

    add routes sauces

--- routes/sauce.js
+++ routes/sauce.js
@@ -5,12 +5,12 @@ const multer = require('../middleware/multer-config');
 
 const sauceCtrl = require('../controllers/sauce');
 
-router.post('/', /*auth,*/sauceCtrl.createSauce);
-router.get('/:id', /*auth,*/ sauceCtrl.getOneSauce);
-router.get('/', /*auth,*/ sauceCtrl.listSauces);
-router.put('/:id', /*auth,*/multer, sauceCtrl.updateSauce);
-router.delete('/:id', /*auth,*/ sauceCtrl.deleteSauce); 
-//router.post('/:id/like', auth, sauceCtrl.likeSauces);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.get('/', auth, sauceCtrl.listSauces);
+router.put('/:id', auth, multer, sauceCtrl.updateSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce); 
+router.post('/:id/like', auth, sauceCtrl.likeSauces);
 
 module.exports = router;
 
