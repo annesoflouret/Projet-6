1cc90c7126ffed0b180c4d0a9cb408cfd9688883
annesoflouret <annesophieflouret@gmail.com>
2020-08-03 13:11:10 +0200

    change token to process.env.token

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, '&VEq2#!WyA#XhTGjwQ2E7Y^@5fRbn7');
+    const decodedToken = jwt.verify(token, process.env.TOKEN);
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
